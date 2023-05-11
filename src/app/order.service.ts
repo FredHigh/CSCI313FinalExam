24a1f64481cf18300c30b933502e5fc364e59d5f
Fred High <fred.high@ndsu.edu>
2023-05-11 02:06:37 -0500

    Added cart and order posting

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { Order } from './order';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  nextId: number = 0;
   cartArray: Product[] = [];
+  totalPrice: number = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNextId() {
+    return this.nextId++;
+  }
 
   addToCart(product: Product) {
     this.cartArray.push(product);
+  }
+
+  priceTotal() {
+    this.cartArray.forEach((product) => {
+
+    });
   }
 
   getCart() {
@@ -20,5 +35,9 @@ export class OrderService {
   removeFromCart(id: number) {
     let index = this.cartArray.findIndex(c => c.product_id === id);
     this.cartArray.splice(index, 1);
+  }
+
+  addOrder(newOrder: Order) {
+    return this.http.post('https://shoppinginfo-4c4b6-default-rtdb.firebaseio.com/' + 'order.json', newOrder);
   }
 }
